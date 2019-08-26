52ad92a6a120fc64d121e82efd70a25987c0be0d
Brendon Fatland <b.fatland@gmail.com>
2019-08-26 15:20:02 -0500

    edited a JSX file to have a function instead of online style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function getButtonText() {
+  return "Click on me!";
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const buttonText = "Click Me!";
+
+  return (
+    <div>
+      <label className="label" for="text">
+        Text
+      </label>
+      <input id="text" />
+      <button style={{ backgroundColor: "red", color: "white" }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
